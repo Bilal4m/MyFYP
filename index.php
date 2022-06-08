692b89576ed5f24f9b420e995de6b1e5b14c8792
Bilal Rasheed <bilal_4m@live.com>
2022-06-09 00:44:51 +0500

    first commit

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ session_start(); //start temp session until logout/browser closed
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                             <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                            <li class="nav-item"> <a class="nav-link active" href="admin/index.php">Admin Panel <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Admin Panel <span class="sr-only"></span></a> </li>
 
                             
                            
